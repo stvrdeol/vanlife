6c1a1f2380f5adce8d543ec3148fc932a79e0a2f
Satvir Singh <stvrdeol@gmail.com>
2023-10-19 11:12:51 +0530

    resolved netlify error by changing the loader import mistake

--- src/pages/Van.jsx
+++ src/pages/Van.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Loader from "../components/loader";
+import Loader from "../components/Loader";
 
 function Van() {
   const param = useParams();
